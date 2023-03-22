84840292525fb752e1fac8f1bf2916c4464a4dbb
Mlockbug <energetichamster@gmail.com>
2023-03-22 12:41:50 +0000

    room movement

---
+++ Assets/Scripts/MoveLogic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveLogic : MonoBehaviour
+{
+    public GameObject mainUI;
+    public GameObject moveUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadRoom(int room)
+    {
+        SceneManager.LoadScene(room - 1);
+    }
+
+    public void ShowMoveUI()
+    {
+        mainUI.SetActive(false);
+        moveUI.SetActive(true);
+    }
+
+    public void ShowMainUI()
+    {
+        mainUI.SetActive(true);
+        moveUI.SetActive(false);
+    }
+}
