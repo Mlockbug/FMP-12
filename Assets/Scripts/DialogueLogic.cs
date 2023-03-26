42acc78faa61984337be453268adb471c50f6bdc
Mlockbug <energetichamster@gmail.com>
2023-03-26 22:23:28 +0100

    dialogue continued

--- Assets/Scripts/DialogueLogic.cs
+++ Assets/Scripts/DialogueLogic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,22 +9,49 @@ public class DialogueLogic : MonoBehaviour
     public GameObject textBox;
     public GameObject mainUI;
     public Text textDisplay;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Text nameDisplay;
+    Queue<string> fullDialogue = new Queue<string>();
+	string diagString;
+    bool continueText;
+    public GameObject cont;
+
+    public void Speak(Dialogue text){
+        continueText = true;
+		mainUI.SetActive(false);
+		textBox.SetActive(true);
+
+        if (fullDialogue != null)
+		    fullDialogue.Clear();
+
+        foreach (string x in text.text)
+            fullDialogue.Enqueue(x);
+
+        while (fullDialogue.Count > 0){
+            if (cont.activeSelf == true && Input.GetKeyDown(KeyCode.Return)){
+                cont.SetActive(false);
+                continueText = true;
+            }
+
+            if (continueText){
+                textDisplay.text = "";
+                diagString = fullDialogue.Dequeue();
+                if (diagString.Contains("NAME-"))
+                    nameDisplay.text = diagString.Split('-')[1];
+                else
+                    StartCoroutine(ReadText());
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ReadText()
     {
-        
-    }
-
-    public void Test(Dialogue text)
-    {
-        mainUI.SetActive(false);
-        textBox.SetActive(true);
-        textDisplay.text = text.text[0];
+        continueText = false;
+		foreach (char x in diagString)
+		{
+			textDisplay.text += x;
+			if (x == ' ')
+                yield return new WaitForSeconds(0.1f);
+		}
+        cont.SetActive(true);
     }
 }
