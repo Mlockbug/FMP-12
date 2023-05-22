800c31c7b2a6e41ce441a90cadcad3be7f463e43
Mlockbug <energetichamster@gmail.com>
2023-05-22 15:34:25 +0100

    start cutscene start

---
+++ Assets/Scripts/OpeningCutscene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningCutscene : MonoBehaviour
+{
+    public RawImage blackScreen;
+    Color screenColor;
+    public GameObject mainUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
